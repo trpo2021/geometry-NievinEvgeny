809641ba4064f579b7908222df2dff0c5514e9f7
NievinEvgeny <zhenya-nievin@mail.ru>
2021-02-20 17:47:05 +0700

    Added functions for parsing instead of while cycles; Сompletely changed the construction of the code

--- geometry.c
+++ geometry.c
@@ -3,7 +3,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-void skipthespace(char* str, int* i)
+void SkipUntilSpaceOrBracket(char* str, int* i)
+{
+    while ((isspace(str[*i]) == 0) && (str[*i] != ')'))
+    {
+        *i = *i + 1;
+    }
+}
+
+void SkipUntilSpaceOrComma(char* str, int* i)
+{
+    while ((isspace(str[*i]) == 0) && (str[*i] != ','))
+    {
+        *i = *i + 1;
+    }
+}
+
+void SkipUntilSpace(char* str, int* i)
+{
+    while (isspace(str[*i]) == 0)
+    {
+        *i = *i + 1;
+    }
+}
+
+void SkipTheSpace(char* str, int* i)
 {
     while (isspace(str[*i]))
     {
@@ -31,137 +55,123 @@ int main()
 
     fgets(str, 50, stdin);
 
-    skipthespace(str, &i);
+    SkipTheSpace(str, &i);
 
-    if (strncmp(&str[i], circle, 6) == 0)
+    if (strncmp(&str[i], circle, 6) != 0)
     {
-        i = i + 6;
-        skipthespace(str, &i);
-        if (str[i] == '(')
-        {
-            i++;
-            skipthespace(str, &i);
+        printf("Ошибка в имени фигуры: Ожидается 'circle'\n");
+        return 0;
+    }
 
-            if (isdigit(str[i]))
-            {
-                char* strtodhelper1 = &str[i];
-                double x = 0;
-                x = strtod(strtodhelper1, &strtodhelper1);
-                printf("%f\n", x);
+    i = i + 6;
+    SkipTheSpace(str, &i);
 
-                if (*strtodhelper1 != ' ')
-                {
-                    printf("Ошибка в синтаксисе: Ожидается ' ' после первой координаты\n");
-                }
-                else
-                {
-                    while (str[i] != ' ')
-                    {
-                        i++;
-                    }
-                    skipthespace(str, &i);
-                    if (isdigit(str[i]))
-                    {
-                        char* strtodhelper2 = &str[i];
-                        double y = 0;
-                        y = strtod(strtodhelper2, &strtodhelper2);
-                        printf("%f\n", y);
+    if (str[i] != '(')
+    {
+        printf("Ошибка в синтаксисе: Ожидается '(' после 'circle'\n");
+        return 0;
+    }
 
-                        if ((*strtodhelper2 != ' ') && (*strtodhelper2 != ','))
-                        {
-                            printf("Ошибка в синтаксисе: Ожидается ' ' или ',' после второй координаты\n");
-                        }
-                        else
-                        {
-                            while ((str[i] != ' ') && (str[i] != ','))
-                            {
-                                i++;
-                            }
-                            skipthespace(str, &i);
-                            if (str[i] != ',')
-                            {
-                                printf("Ошибка в синтаксисе: Ожидается ',' после второй координаты\n");
-                            }
-                            else
-                            {
-                                i++;
-                                skipthespace(str, &i);
-                                if (isdigit(str[i]))
-                                {
-                                    char* strtodhelper3 = &str[i];
-                                    double radius = 0;
-                                    radius = strtod(strtodhelper3, &strtodhelper3);
-                                    printf("%f\n", radius);
+    i++;
+    SkipTheSpace(str, &i);
 
-                                    if ((*strtodhelper3 != ' ') && (*strtodhelper3 != ')'))
-                                    {
-                                        printf("Ошибка в синтаксисе: Ожидается ' ' или ')' после радиуса\n");
-                                    }
-                                    else
-                                    {
-                                        while ((str[i] != ' ') && (str[i] != ')'))
-                                        {
-                                            i++;
-                                        }
-                                        skipthespace(str, &i);
-                                        if (str[i] != ')')
-                                        {
-                                            printf("Ошибка в синтаксисе: Ожидается ')' после радиуса\n");
-                                        }
-                                        else
-                                        {
-                                            i++;
-                                            if (str[i] == '\n')
-                                            {
-                                                printf("Данные введены верно\n");
-                                            }
-                                            else
-                                            {
-                                                if (str[i] == ' ')
-                                                {
-                                                    skipthespace(str, &i);
-                                                    if (str[i] == '\n')
-                                                    {
-                                                        printf("Данные введены верно\n");
-                                                    }
-                                                    else
-                                                    {
-                                                        printf("Ошибка в синтаксисе: Ожидается 'символ конца строки' после ')'\n");
-                                                    }
-                                                }
-                                                else
-                                                {
-                                                    printf("Ошибка в синтаксисе: Ожидается 'символ конца строки' после ')'\n");
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                                else
-                                {
-                                    printf("Ошибка в синтаксисе: Ожидается радиус после ','\n");
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        printf("Ошибка в синтаксисе: Ожидается вторая координата после первой координаты\n");
-                    }
-                }
-            }
-            else
-            {
-                printf("Ошибка в синтаксисе: Ожидается координата после '('\n");
-            }
-        }
-        else
-        {
-            printf("Ошибка в синтаксисе: Ожидается '(' после 'circle'\n");
-        }
+    if (isdigit(str[i]) == 0)
+    {
+        printf("Ошибка в синтаксисе: Ожидается координата после '('\n");
+        return 0;
+    }
+
+    char* strtodhelper1 = &str[i];
+    double x = 0;
+    x = strtod(strtodhelper1, &strtodhelper1);
+    printf("%f\n", x);
+
+    if (*strtodhelper1 != ' ')
+    {
+        printf("Ошибка в синтаксисе: Ожидается ' ' после первой координаты\n");
+        return 0;
+    }
+
+    SkipUntilSpace(str, &i);
+    SkipTheSpace(str, &i);
+
+    if (isdigit(str[i]) == 0)
+    {
+        printf("Ошибка в синтаксисе: Ожидается вторая координата после первой координаты\n");
+        return 0;
+    }
+
+    char* strtodhelper2 = &str[i];
+    double y = 0;
+    y = strtod(strtodhelper2, &strtodhelper2);
+    printf("%f\n", y);
+
+    if ((*strtodhelper2 != ' ') && (*strtodhelper2 != ','))
+    {
+        printf("Ошибка в синтаксисе: Ожидается ' ' или ',' после второй координаты\n");
+        return 0;
+    }
+
+    SkipUntilSpaceOrComma(str, &i);
+    SkipTheSpace(str, &i);
+
+    if (str[i] != ',')
+    {
+        printf("Ошибка в синтаксисе: Ожидается ',' после второй координаты\n");
+        return 0;
+    }
+
+    i++;
+    SkipTheSpace(str, &i);
+
+    if (isdigit(str[i]) == 0)
+    {
+        printf("Ошибка в синтаксисе: Ожидается радиус после ','\n");
+        return 0;
+    }
+
+    char* strtodhelper3 = &str[i];
+    double radius = 0;
+    radius = strtod(strtodhelper3, &strtodhelper3);
+    printf("%f\n", radius);
+
+    if ((*strtodhelper3 != ' ') && (*strtodhelper3 != ')'))
+    {
+        printf("Ошибка в синтаксисе: Ожидается ' ' или ')' после радиуса\n");
+        return 0;
+    }
+
+    SkipUntilSpaceOrBracket(str, &i);
+    SkipTheSpace(str, &i);
+
+    if (str[i] != ')')
+    {
+        printf("Ошибка в синтаксисе: Ожидается ')' после радиуса\n");
+        return 0;
+    }
+
+    i++;
+
+    if (str[i] == '\n')
+    {
+        printf("Данные введены верно\n");
+    }
+
+    if (isspace(str[i]) == 0)
+    {
+        printf("Ошибка в синтаксисе: Ожидается 'символ конца строки' после ')'\n");
+        return 0;
+    }
+
+    SkipTheSpace(str, &i);
+
+    if (str[i] != '\n')
+    {
+        printf("Ошибка в синтаксисе: Ожидается 'символ конца строки' после ')'\n");
+        return 0;
     }
     else
     {
-        printf("Ошибка в имени фигуры: Ожидается 'circle'\n");
+        printf("Данные введены верно\n");
     }
 }
