f1ee8958970c0764d58279dea623b775a7bb9ad6
NievinEvgeny <zhenya-nievin@mail.ru>
2021-02-27 17:43:21 +0700

    Fix the value returned by the function. The cycle ends when incorrect data is entered

--- geometry.c
+++ geometry.c
@@ -13,7 +13,6 @@ void SkipSpace(char* str, int* i)
 
 int CorrectWritingCircle(char* str, int* i)
 {
-    int k = -1;
     char Circle[] = {"circle"};
     int LengthOfCircle = strlen(Circle);
     SkipSpace(str, i);
@@ -21,7 +20,7 @@ int CorrectWritingCircle(char* str, int* i)
     if (strncmp(&str[*i], Circle, LengthOfCircle) != 0)
     {
         printf("Ошибка в имени фигуры: Ожидается 'circle'\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + LengthOfCircle;
@@ -30,7 +29,7 @@ int CorrectWritingCircle(char* str, int* i)
     if (str[*i] != '(')
     {
         printf("Ошибка в синтаксисе: Ожидается '(' после 'circle'\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + 1;
@@ -43,7 +42,7 @@ int CorrectWritingCircle(char* str, int* i)
     if ((x == 0) && (LetterAfterFirstCoord == &str[*i]))
     {
         printf("Ошибка в синтаксисе: Ожидается координата после '('\n");
-        return 0;
+        return -1;
     }
 
     printf("%f\n", x);
@@ -51,7 +50,7 @@ int CorrectWritingCircle(char* str, int* i)
     if (*LetterAfterFirstCoord != ' ')
     {
         printf("Ошибка в синтаксисе: Ожидается ' ' после первой координаты\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + LetterAfterFirstCoord - &str[*i];
@@ -64,7 +63,7 @@ int CorrectWritingCircle(char* str, int* i)
     if ((y == 0) && (LetterAfterSecondCoord == &str[*i]))
     {
         printf("Ошибка в синтаксисе: Ожидается вторая координата после первой координаты\n");
-        return 0;
+        return -1;
     }
 
     printf("%f\n", y);
@@ -72,7 +71,7 @@ int CorrectWritingCircle(char* str, int* i)
     if ((*LetterAfterSecondCoord != ' ') && (*LetterAfterSecondCoord != ','))
     {
         printf("Ошибка в синтаксисе: Ожидается ' ' или ',' после второй координаты\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + LetterAfterSecondCoord - &str[*i];
@@ -81,7 +80,7 @@ int CorrectWritingCircle(char* str, int* i)
     if (str[*i] != ',')
     {
         printf("Ошибка в синтаксисе: Ожидается ',' после второй координаты\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + 1;
@@ -94,13 +93,13 @@ int CorrectWritingCircle(char* str, int* i)
     if ((Radius == 0) && (LetterAfterRadius == &str[*i]))
     {
         printf("Ошибка в синтаксисе: Ожидается радиус после ','\n");
-        return 0;
+        return -1;
     }
 
     if (Radius < 0)
     {
         printf("Ошибка: Радиус не может быть отрицательным\n");
-        return 0;
+        return -1;
     }
 
     printf("%f\n", Radius);
@@ -108,7 +107,7 @@ int CorrectWritingCircle(char* str, int* i)
     if ((*LetterAfterRadius != ' ') && (*LetterAfterRadius != ')'))
     {
         printf("Ошибка в синтаксисе: Ожидается ' ' или ')' после радиуса\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + LetterAfterRadius - &str[*i];
@@ -117,7 +116,7 @@ int CorrectWritingCircle(char* str, int* i)
     if (str[*i] != ')')
     {
         printf("Ошибка в синтаксисе: Ожидается ')' после радиуса\n");
-        return 0;
+        return -1;
     }
 
     *i = *i + 1;
@@ -127,15 +126,14 @@ int CorrectWritingCircle(char* str, int* i)
     if (str + *i != str + strlen(str))
     {
         printf("Ошибка в синтаксисе: ожидается 'символ конца строки' после ')'\n");
-        return 0;
+        return -1;
     }
     else
     {
         printf("Данные введены верно, переход к новой строке...\n");
     }
-    k++;
     *i = 0;
-    return k;
+    return 0;
 }
 
 int main()
@@ -151,7 +149,7 @@ int main()
     if (InputData == NULL)
     {
         printf("ошибка\n");
-        return 0;
+        return -1;
     }
     else
     {
